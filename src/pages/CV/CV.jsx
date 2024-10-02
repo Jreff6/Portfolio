c3a3c171166daccae43c74d3a4a5747e7597c36d
Jreff6 <enzo.solaar@hotmail.fr>
2024-10-02 10:32:16 +0200

    updated CV host after a bug

--- src/pages/CV/CV.jsx
+++ src/pages/CV/CV.jsx
@@ -9,7 +9,7 @@ function CV() {
   return (
     <div className='CVWrapper'>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <Viewer fileUrl='/CV.pdf' defaultScale={SpecialZoomLevel.PageFit} className='pdf-viewer' />
+        <Viewer fileUrl='https://www.dropbox.com/scl/fi/q4witkg2202vf6vwvzemv/CV.pdf?rlkey=yk0fz9m21rlw2ke0mysbb318l&st=w0m3bru8&dl=1' defaultScale={SpecialZoomLevel.PageFit} className='pdf-viewer' />
       </Worker>
     </div>
   );
